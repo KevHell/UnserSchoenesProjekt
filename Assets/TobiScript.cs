1c48e12a293108b9f75d8da42a7463aa54326b65
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-07-16 14:28:54 +0200

    debug log

---
+++ Assets/TobiScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TobiScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("super fun!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
