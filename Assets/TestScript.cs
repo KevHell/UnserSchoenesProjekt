593859d34f71c3a53164917854c0ba9081015bdc
Sebastian Fülöp <sebastian.fueloep@gmail.com>
2018-07-16 14:22:18 +0200

    Testscript

---
+++ Assets/TestScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Das ist ein Testscript");
+    }
+    
+}
