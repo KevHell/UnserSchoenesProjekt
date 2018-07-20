4afbe81442d9f2969413bb8e2a8a6f0a67776962
SamAut <philip2000_5@hotmail.com>
2018-07-20 13:27:24 +0200

    huj

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log("Das ist ein Testscript");
+
+        Debug.Log("Mit freundlichen Grüßen,\nNOT-Kevin Hell");
+        //Debug.Log("Ich habe dein Testscript geändert.");
+        //Debug.Log("Mit freundlichen Grüßen,\nNOT-Kevin Hell");
+        Debug.Log("NOT NOT NOT");
+        //Debug.Log("Danke! MfG NOT-Sebastian");
+    }
+    
+}
---
+++ Assets/Scripts/TobiScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TobiScript : MonoBehaviour
+{
+
+   
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("super fun!");
+
+        
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+        Debug.Log("it is green!");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        }
+
+        
+
+
+
+
+    }
+}
