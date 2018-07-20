ed61ff8645ea62ba58f0fd14d11b8e691dc32839
Sebastian Fülöp <sebastian.fueloep@gmail.com>
2018-07-20 13:41:54 +0200

    Perfekte Performance

---
+++ Assets/Scripts/RainDrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDrop : MonoBehaviour
+{
+
+    Vector3 newScale;
+    // Use this for initialization
+    void Start()
+    {
+
+        newScale = transform.localScale;
+
+        Invoke("Destroy", 2f);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
