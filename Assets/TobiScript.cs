68237f7f7c5bc9caed54a917b60995166b93421d
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-07-20 12:51:22 +0200

    green

--- Assets/TobiScript.cs
+++ Assets/TobiScript.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TobiScript : MonoBehaviour {
+public class TobiScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+   
+    // Use this for initialization
+    void Start()
+    {
         Debug.Log("super fun!");
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		
-	}
+        
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+        Debug.Log("it is green!");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        }
+
+        
+
+
+
+
+    }
 }
